6196a0a6dc5e775ae0a4f0580e71c1ed8dcad070
Marco Silva <marcosilva2203@outlook.pt>
2024-11-01 20:22:00 +0000

    Add base math classes and header files

---
+++ Engine/Source/Types.h
@@ -0,0 +1,16 @@
+#pragma once
+
+// Integer types
+typedef signed char                 int8;
+typedef unsigned char               uint8;
+typedef short int                   int16;
+typedef unsigned short int          uint16;
+typedef int                         int32;
+typedef unsigned int                uint32;
+typedef long long int               int64;
+typedef unsigned long long int      uint64;
+
+// Floating point types
+typedef float                       float32;
+typedef double                      float64;
+typedef long double                 float128;   
